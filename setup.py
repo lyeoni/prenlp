34771a29c0582bf4fb9a49d188a60f314ad60ff3
lyeoni <lyeoni.g@gmail.com>
2019-11-12 19:15:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: Apache License'
+        'License :: OSI Approved :: Apache Software License'
     ]
 )
