30ed0470d0f66c666521bdbc60a2287e4682939f
lyeoni <lyeoni.g@gmail.com>
2019-11-15 17:06:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,21 +5,22 @@ with open('README.md', encoding='utf-8') as reader:
     long_description = reader.read()
 
 setuptools.setup(
-    name                = 'prenlp',
-    version             = prenlp.__version__,
-    author              = prenlp.__author__,
-    author_email        = prenlp.__email__,
-    description         = 'Preprocessing Library for Natural Language Processing',
-    long_description    = long_description,
-    url                 = 'https://github.com/lyeoni/nlp-preprocessing',
-    packages            = setuptools.find_packages(),
-    install_requires    = [],
-    package_data        = {},
-    keywords            = [
+    name                            = 'prenlp',
+    version                         = prenlp.__version__,
+    author                          = prenlp.__author__,
+    author_email                    = prenlp.__email__,
+    description                     = 'Preprocessing Library for Natural Language Processing',
+    long_description                = long_description,
+    long_description_content_type   = 'text/markdown',
+    url                             = 'https://github.com/lyeoni/prenlp',
+    packages                        = setuptools.find_packages(),
+    install_requires                = [],
+    package_data                    = {},
+    keywords                        = [
         'nlp',
         'text-preprocessing'
     ],
-    classifiers         = [
+    classifiers                     = [
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
